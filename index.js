a37a52a779901097c55da6959bd2fde812449e78
Erik Chung <ec8377@rit.edu>
2025-02-11 16:03:29 -0500

    implemented square price update system.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { createRequire } from "module";
+import { SquareClient } from "square";
 
 const require = createRequire(import.meta.url);
 require("dotenv").config();
@@ -13,9 +14,44 @@ const path = require("path");
 const process = require('node:process');
 const { json } = require("stream/consumers");
 
+const client = new SquareClient({ token: process.env.TOKEN });
+const id_name_dict = {};
+const item_cost = {};
+
 const PROCESS_DIR = process.cwd();  
 let menu_counter = parseInt(await fspromise.readFile(path.join(PROCESS_DIR, "resources", "menu", "menu_counter.txt")));
 const json_backup_data = await fspromise.readFile(PROCESS_DIR + "/resources/menu/menu_BACKUP.json", "utf-8");
+let json_data = await fspromise.readFile(PROCESS_DIR + "/resources/menu/menu.json", "utf-8");
+let json_object = JSON.parse(json_data);
+
+let response = await client.catalog.list({types:["ITEM"]});
+for await (const item of response) {
+    id_name_dict[item.id] = item.itemData.name;
+}
+
+response = await client.catalog.list({types: "ITEM_VARIATION"});
+for await (const item of response) {
+    if (item_cost[id_name_dict[item.itemVariationData.itemId]] != undefined && item_cost[id_name_dict[item.itemVariationData.itemId]] > item.itemVariationData.priceMoney.amount) {
+        item_cost[id_name_dict[item.itemVariationData.itemId]] = item.itemVariationData.priceMoney.amount
+    }
+    else if (item_cost[id_name_dict[item.itemVariationData.itemId]] == undefined) {
+        item_cost[id_name_dict[item.itemVariationData.itemId]] = item.itemVariationData.priceMoney.amount
+    }
+}
+
+json_object.categories.forEach((category) => {
+    category.items.forEach((item) => {
+        if (item_cost[item.name] != undefined) {
+            let temp = new Number(item_cost[item.name]).toString();
+            let cost_string = temp.toString().substring(0,temp.length-2) + "." + temp.toString().substring(temp.length-2);
+            item.cost = cost_string;
+        }
+    })
+})
+
+console.log(item_cost)
+
+await fspromise.writeFile(PROCESS_DIR + "/resources/menu/menu.json", JSON.stringify(json_object));
 
 async function check_path(req) {
     if ((req.path.indexOf(".html") >= 0) || (req.path.indexOf(".js") >= 0) || (req.path.indexOf(".json") >= 0) || (req.path.indexOf(".txt") >= 0)) {
@@ -114,8 +150,43 @@ app.post("/JSON_UPLOAD", async (req, res) => {
     await fspromise.writeFile(PROCESS_DIR + "/resources/menu/menu.json", req.body.JSON_UPLOAD);
     let html =  await fspromise.readFile(PROCESS_DIR + "/menu_changer.html","utf-8");
     let json_data = await fspromise.readFile(PROCESS_DIR + "/resources/menu/menu.json", "utf-8");
-    res.writeHead(200, "")
     res.send(html.replaceAll("REPLACE_JSON_STRING", json_data.replaceAll("\n","").replaceAll("'", "\\'")));
+});
+
+app.post("/SQUARE_UPDATE", async (req, res) => {
+    let html =  await fspromise.readFile(PROCESS_DIR + "/menu_changer.html","utf-8");
+    let json_data = await fspromise.readFile(PROCESS_DIR + "/resources/menu/menu.json", "utf-8");
+    let json_object = JSON.parse(json_data);
+
+    let response = await client.catalog.list({types:["ITEM"]});
+    for await (const item of response) {
+        id_name_dict[item.id] = item.itemData.name;
+    }
+
+    response = await client.catalog.list({types: "ITEM_VARIATION"});
+    for await (const item of response) {
+        if (item_cost[id_name_dict[item.itemVariationData.itemId]] != undefined && item_cost[id_name_dict[item.itemVariationData.itemId]] > item.itemVariationData.priceMoney.amount) {
+            item_cost[id_name_dict[item.itemVariationData.itemId]] = item.itemVariationData.priceMoney.amount
+        }
+        else if (item_cost[id_name_dict[item.itemVariationData.itemId]] == undefined) {
+            item_cost[id_name_dict[item.itemVariationData.itemId]] = item.itemVariationData.priceMoney.amount
+        }
+    }
+
+
+    json_object.categories.forEach((category) => {
+        category.items.forEach((item) => {
+            if (item_cost[item.name] != undefined) {
+                let temp = new Number(item_cost[item.name]).toString();
+                let cost_string = temp.toString().substring(0,temp.length-2) + "." + temp.toString().substring(temp.length-2);
+                item.cost = cost_string;
+            }
+        })
+    })
+
+    await fspromise.writeFile(PROCESS_DIR + "/resources/menu/menu.json", JSON.stringify(json_object));
+
+    res.send(html.replaceAll("REPLACE_JSON_STRING", JSON.stringify(json_object).replaceAll("\n","").replaceAll("'", "\\'")));
 });
 
 // don't touch beyond this bruh
@@ -135,5 +206,5 @@ function rand_int(max) {
     return Math.floor(Math.random() * max);
 }
 
-var httpserver = http.createServer(app);
-httpserver.listen(80);
+// var httpserver = http.createServer(app);
+// httpserver.listen(80);
